d3f56973a9d9282b332d5df0385a51f6043d1725
Belen <bemartin@hotmail.com.ar>
2022-10-19 18:59:35 -0300

    cs

---
+++ ejercicio 3-2/ejercicio_3_2_4.py
@@ -0,0 +1,35 @@
+class Contacto:
+    def __init__(self, nombre, telefono):
+        self.nombre = nombre
+        self.telefono = telefono
+
+    def __str__(self):
+        return "{},{}".format(self.nombre, self.telefono)
+
+
+class Agenda:
+    def __init__(self, contactos, cantidad_de_contactos =10):
+        self.contactos = contactos
+        self.cantidad_de_contactos = cantidad_de_contactos
+
+
+    def anadirContacto(self, contacto):
+        pass
+
+    def existeContacto(self, contacto):
+        pass
+
+    def listarContactos(self):
+        pass
+
+    def buscaContacto(self, stringNombre):
+        pass
+
+    def eliminarContacto(self, contacto):
+        pass
+
+    def agendaLlena(self):
+        pass
+
+    def huecosLibres(self):
+        pass
