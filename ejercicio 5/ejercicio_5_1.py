4a751b82b7bb8723d5c9861c72ae97cece5ba54b
Belen <bemartin@hotmail.com.ar>
2022-10-19 20:21:47 -0300

    ejercicio 5

---
+++ ejercicio 5/ejercicio_5_1.py
@@ -0,0 +1,21 @@
+class Instrumento:
+    def __init__(self, precio, marca, modelo):
+        self.precio = precio
+        self.marca = marca
+        self.modelo = modelo
+
+    def tocar(self):
+        pass
+
+class Guitarra(Instrumento):
+    def __init__(self, precio, marca, modelo):
+        Punto.__init__(self, x, y)
+        super().__init__(x, y)
+        self.z = z
+
+class Bateria(Instrumento):
+    pass
+
+class Piano(Instrumento):
+    pass
+
